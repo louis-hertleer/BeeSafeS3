520c357cb07bb4470648f5307454444325937383
Yussef Dalton <r0930164@student.thomasmore.be>
2025-01-15 12:33:17 +0100

    Refactor validation code into seperate method
    This is for code reuse.

--- BeeSafeWeb/BeeSafeWeb/Controllers/DeviceController.cs
+++ BeeSafeWeb/BeeSafeWeb/Controllers/DeviceController.cs
@@ -39,15 +39,14 @@ public class DeviceController : Controller
         return Ok(response);
     }
 
-    [HttpPost("Ping")]
-    public IActionResult Ping(RequestMessage requestMessage)
+    private IActionResult MessageIsValid(RequestMessage message)
     {
         Device? device;
         Guid guid;
 
-        if (String.IsNullOrEmpty(requestMessage.Device) || !Guid.TryParse(requestMessage.Device, out guid))
+        if (String.IsNullOrEmpty(message.Device) || !Guid.TryParse(message.Device, out guid))
         {
-            return BadRequest();
+            return Unauthorized();
         }
 
         device = _deviceRepository.GetById(guid);
@@ -61,6 +60,24 @@ public class DeviceController : Controller
         if (!device.IsApproved)
         {
             return StatusCode(403);
+        }
+
+        return Ok();
+    }
+
+    [HttpPost("Ping")]
+    public IActionResult Ping(RequestMessage requestMessage)
+    {
+        var result = MessageIsValid(requestMessage);
+
+        if (result is not OkResult)
+        {
+            return result;
+        }
+
+        if (requestMessage.MessageType != MessageType.Ping)
+        {
+            return BadRequest();
         }
 
         var response = new ResponseMessage()
@@ -74,25 +91,16 @@ public class DeviceController : Controller
     [HttpPost("DetectionEvent")]
     public IActionResult DetectionEvent(RequestMessage requestMessage)
     {
-        Device? device;
-        Guid guid;
+        var result = MessageIsValid(requestMessage);
 
-        if (String.IsNullOrEmpty(requestMessage.Device) || !Guid.TryParse(requestMessage.Device, out guid))
+        if (result is not OkResult)
+        {
+            return result;
+        }
+
+        if (requestMessage.MessageType != MessageType.DetectionEvent)
         {
             return BadRequest();
-        }
-
-        device = _deviceRepository.GetById(guid);
-
-        /* device must be in the system */
-        if (device == null)
-        {
-            return Unauthorized();
-        }
-
-        if (!device.IsApproved)
-        {
-            return StatusCode(403);
         }
 
         return Ok();
