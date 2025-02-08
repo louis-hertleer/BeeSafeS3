f16d3a165ec18d5dd5d7e502930d39f43e4a5fa2
Moussa Ramzi <r0881426@student.thomasmore.be>
2025-02-08 21:30:42 +0100

    nestestemste on homepage

--- BeeSafeWeb/BeeSafeWeb/Controllers/HomeController.cs
+++ BeeSafeWeb/BeeSafeWeb/Controllers/HomeController.cs
@@ -6,60 +6,67 @@ using BeeSafeWeb.Utility.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using System.Diagnostics;
+using BeeSafeWeb.Services;
 
-namespace BeeSafeWeb.Controllers;
-
-[Authorize]
-public class HomeController : Controller
+namespace BeeSafeWeb.Controllers
 {
-    private readonly IRepository<Device> _deviceRepository;
-    private readonly IRepository<NestEstimate> _nestEstimateRepository;
-
-    public HomeController(IRepository<Device> deviceRepository, IRepository<NestEstimate> nestEstimateRepository)
+    [Authorize]
+    public class HomeController : Controller
     {
-        _deviceRepository = deviceRepository;
-        _nestEstimateRepository = nestEstimateRepository;
-    }
+        private readonly IRepository<Device> _deviceRepository;
+        private readonly IRepository<NestEstimate> _nestEstimateRepository;
+        private readonly NestLocationService _nestLocationService;
 
-    public async Task<IActionResult> Index()
-    {
-        var devices = await Task.Run(() => _deviceRepository.GetQueryable()
-            .Where(d => d.IsApproved && !d.IsDeclined)
-            .Select(d => new
-            {
-                d.Id,
-                d.Latitude,
-                d.Longitude,
-                d.IsOnline,
-                d.IsTracking,
-                d.LastActiveString
-            }).ToList());
+        public HomeController(
+            IRepository<Device> deviceRepository, 
+            IRepository<NestEstimate> nestEstimateRepository,
+            NestLocationService nestLocationService)
+        {
+            _deviceRepository = deviceRepository;
+            _nestEstimateRepository = nestEstimateRepository;
+            _nestLocationService = nestLocationService;
+        }
 
-        var nestEstimates = (await _nestEstimateRepository.GetAllAsync())
-            .Select(n => new
+        public async Task<IActionResult> Index()
+        {
+            // Retrieve devices that are approved and not declined.
+            var devices = await Task.Run(() => _deviceRepository.GetQueryable()
+                .Where(d => d.IsApproved && !d.IsDeclined)
+                .Select(d => new
+                {
+                    d.Id,
+                    d.Latitude,
+                    d.Longitude,
+                    d.IsOnline,
+                    d.IsTracking,
+                    d.LastActiveString
+                }).ToList());
+
+            // Use the same nest location calculation as in the Visitors controller.
+            // This method returns aggregated nest estimates.
+            var nestEstimates = await _nestLocationService.CalculateNestLocationsAsync();
+
+            // Project the nest data into an anonymous type.
+            // Note: We use the aggregated display properties if available.
+            var nestData = nestEstimates.Select(n => new
             {
-                n.EstimatedLatitude,
-                n.EstimatedLongitude,
-                n.AccuracyLevel,
-                n.IsDestroyed,
-                LastUpdatedString = n.LastUpdatedString // Add the formatted string
+                lat = n.DisplayLatitude ?? n.EstimatedLatitude,
+                lng = n.DisplayLongitude ?? n.EstimatedLongitude,
+                radius = n.DisplayAccuracy ?? n.AccuracyLevel,
+                LastUpdatedString = n.LastUpdatedString, // Formatted string for last update
+                IsDestroyed = n.IsDestroyed
             }).ToList();
 
-        ViewData["Devices"] = devices;
-        ViewData["NestEstimates"] = nestEstimates;
+            ViewData["Devices"] = devices;
+            ViewData["NestEstimates"] = nestData;
 
-        return View();
-    }
+            return View();
+        }
 
+        public IActionResult Privacy() => View();
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() =>
+            View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
