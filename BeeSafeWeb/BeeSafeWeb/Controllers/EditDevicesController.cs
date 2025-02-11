03e538f7e7584f7bac6d56dbe16703006b827d63
Yussef Dalton <r0930164@student.thomasmore.be>
2025-02-11 11:29:59 +0100

    Add edit devices page

---
+++ BeeSafeWeb/BeeSafeWeb/Controllers/EditDevicesController.cs
@@ -0,0 +1,57 @@
+using BeeSafeWeb.Data;
+using BeeSafeWeb.Utility.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeeSafeWeb.Controllers;
+
+[Authorize]
+public class EditDevicesController : Controller
+{
+    private readonly IRepository<Device> _deviceRepository;
+
+    public EditDevicesController(IRepository<Device> deviceRepository)
+    {
+        _deviceRepository = deviceRepository;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var devices = await _deviceRepository.GetQueryable()
+            .Select(d => new
+            {
+                d.Id,
+                d.Latitude,
+                d.Longitude,
+                d.Direction,
+                d.IsOnline,
+                d.IsTracking,
+                d.LastActiveString
+            }).ToListAsync();
+
+        return View(devices);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update(Guid id, [FromForm] ApprovalsController.DeviceApprovalModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var device = await _deviceRepository.GetByIdAsync(id);
+        if (device == null)
+        {
+            return NotFound();
+        }
+
+        device.Latitude = model.Latitude;
+        device.Longitude = model.Longitude;
+        device.Direction = model.Direction;
+
+        await _deviceRepository.UpdateAsync(device);
+        return RedirectToAction(nameof(Index));
+    }
+}
